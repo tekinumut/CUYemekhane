0244e4348a7f563f502dac6f22eef438a055dfe7
Umut Tekin <bm.umuttekin@gmail.com>
2023-02-06 01:14:26 +0300

    Add splash screen

--- app/src/main/java/com/tekinumut/cuyemekhane/main/MainActivity.kt
+++ app/src/main/java/com/tekinumut/cuyemekhane/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tekinumut.cuyemekhane.main
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -20,6 +21,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         init()
